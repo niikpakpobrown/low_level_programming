d36e578bdc94dee2aa120084409a05330dc9d5ec
niikpakpobrown <brown.niikpakpo@gmail.com>
2022-04-13 23:22:39 +0000

    The header file was edited

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,13 @@
 #ifndef FILE_FUNCTION
 #define FILE_FUNCTION
+#include <stdio.h>
+#include <stdlib.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
 
 #endif
